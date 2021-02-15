6b84158cc48678824dc8a8eb05baa69b260f0990
Eliaz Bobadilla <eliaz.bobadilladev@gmailcom>
2021-02-15 08:24:42 -0500

    My first cool selenium suit

---
+++ Code/03_FirstSuit/smoke_tests.py
@@ -0,0 +1,18 @@
+from unittest import TestLoader, TestSuite
+from pyunitreport import HTMLTestRunner
+from assertions import AssertionsTest
+from search_test import SearchTests
+
+assertions_test = TestLoader().loadTestsFromTestCase(AssertionsTest)
+search_tests = TestLoader().loadTestsFromTestCase(SearchTests)
+
+smoke_test = TestSuite([assertions_test, search_tests])
+
+
+kwargs = {
+    "output": 'smoke-report'
+}
+
+
+runner = HTMLTestRunner(**kwargs)
+runner.run(smoke_test)
