2075c0cc2008341f98ba05bb00cb273838311509
Eliaz Bobadilla <eliaz.bobadilladev@gmailcom>
2021-02-20 18:42:09 -0500

    Commit

---
+++ Code/08_tee.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+
+class CompareProducts(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = './chromedriver')
+        driver = self.driver
+        driver.implicity_wait(30)
+        driver.maximize_window()
+        driver.get('http://demo-store.seleniumacademy.com')
+        
+
+    def test_compare_products_removal_alerts(self):
+        driver = self.driver
+        search_field = driver.find_elements_by_name('q')
+        search_field.clear()
+
+        search_field.send_keys('tee')
+        search_field.submit()
+
+        driver.find_element_by_class_name('link-compare').click()
+        driver.find_elements_by_link_text('Clear All').click()
+
+        alert = driver.switch_to_alert()
+        alert_text = alert.text
+
+        self.assertEqual('Are you sure you would like to remove all products from your comparison?', alert_text)
+
+        alert.accept()
+
+if __name__=='__main__':	
+    unittest.main(verbosity = 2)
